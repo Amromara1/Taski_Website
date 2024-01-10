d7690abecb49e3b640777ec2b9f41c9ee05b4810
Amr Omara <amromara.civil@gmail.com>
2024-01-10 19:29:47 +0100

    Projektdateien hinzufügen.

---
+++ Taski_Website/Model/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace Taski_Website.Model
+{
+    public class Task
+    {
+        public string TaskId { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public string TaskStatus { get; set; }
+        public DateTime DueDate { get; set; }
+        public List<User> user {  get; set; }
+
+    }
+}
